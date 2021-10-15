71e4987851def4a79c593630dec1a6ab0a7611ab
karamanmertcan <20181701056@stu.khas.edu.tr>
2021-10-15 18:16:18 +0300

    some changes

--- components/Landing.js
+++ components/Landing.js
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import img from '../public/images/hero_2.jpg';
 
 import DemoCarousel from './Carousel';
@@ -10,12 +9,5 @@ const Landing = () => {
     </>
   );
 };
-
-// const Carousel = styled.div`
-//   width: 100%;
-//   height: 100vh;
-//   background-image: url(https://www.womdanismanlik.com/images/hero_2.jpg);
-//   background-size: cover;
-// `;
 
 export default Landing;
