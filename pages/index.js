a41dc77076c7e83678c77508af2a734a84758fe6
karamanmertcan <20181701056@stu.khas.edu.tr>
2021-10-20 14:35:14 +0300

    some bugs

--- pages/index.js
+++ pages/index.js
@@ -344,7 +344,7 @@ const Home = ({ data }) => {
                 Blog Yazıları
               </h2>
               <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-                {data?.map((item, index) => {
+                {data.map((item, index) => {
                   const { title, publishedAt, mainImage, excerpt, slug } = item;
                   return (
                     <BlogCard
