29d822a12213cffd5b535a3e53967665aacb5b2e
ferhatosanoglu <ferhatosanogluo@gmail.com>
2021-10-26 17:53:24 +0300

    respons image

--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,19 @@
 import Layout from '../components/Layout';
+import Image from 'next/image';
 
 const Login = () => {
-  return (
-    <Layout>
-      <section className="container mt-5 pt-5" style={{ height: '100vh', width: '100%' }}>
-        <div className="row">
-          <div className="col-md-6 d-flex  justify-content-center">Selam</div>
-          <div className="col-md-6">Naber halin keyfin</div>
-        </div>
-      </section>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <section className="container mt-5 pt-5" style={{ height: '100vh', width: '100vw' }}>
+                <div className="row">
+                    <div className="col-md-6 h-100"> naber    </div>
+                    <div className="col-md-6 h-100">
+                        <Image src={"/images/logo.png"} style={{ height: '100% !important', width: '100% !important' }} height={200} width={150} objectFit='cover' />
+                    </div>
+                </div>
+            </section>
+        </Layout>
+    );
 };
 
 export default Login;
