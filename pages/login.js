8488a81915b8c90c1c9da5c0bb6ebda78907d930
karamanmertcan <20181701056@stu.khas.edu.tr>
2021-10-26 18:15:03 +0300

    image cover all column

--- pages/login.js
+++ pages/login.js
@@ -2,18 +2,24 @@ import Layout from '../components/Layout';
 import Image from 'next/image';
 
 const Login = () => {
-    return (
-        <Layout>
-            <section className="container mt-5 pt-5" style={{ height: '100vh', width: '100vw' }}>
-                <div className="row">
-                    <div className="col-md-6 h-100"> naber    </div>
-                    <div className="col-md-6 h-100">
-                        <Image src={"/images/logo.png"} style={{ height: '100% !important', width: '100% !important' }} height={200} width={150} objectFit='cover' />
-                    </div>
-                </div>
-            </section>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <section className="container mt-5 pt-5 pb-5" style={{ height: 'auto', width: '100vw' }}>
+        <div className="row">
+          <div className="col-md-6 h-100"> naber </div>
+          <div className="col-md-6 h-100 ">
+            <Image
+              src={'/images/logo.png'}
+              height={200}
+              width={200}
+              objectFit="contain"
+              layout="responsive"
+            />
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
 export default Login;
