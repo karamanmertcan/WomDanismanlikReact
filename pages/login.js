1830618bc436d38d1ca4031153fddfa241647ecf
karamanmertcan <20181701056@stu.khas.edu.tr>
2021-10-26 17:29:58 +0300

    login page fixed

--- pages/login.js
+++ pages/login.js
@@ -1,19 +1,16 @@
-
 import Layout from '../components/Layout';
 
-const pagestyle = {
-    width: '100vw',
-    heigth: '100vh',
-}
 const Login = () => {
-    return (
-        <Layout style={pagestyle}>
-            <div className="col-6 h-100">
-                hello word
-            </div>
-            <div className="col-6 h-100"></div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <section className="container mt-5 pt-5" style={{ height: '100vh', width: '100%' }}>
+        <div className="row">
+          <div className="col-md-6 d-flex  justify-content-center">Selam</div>
+          <div className="col-md-6">Naber halin keyfin</div>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
 export default Login;
