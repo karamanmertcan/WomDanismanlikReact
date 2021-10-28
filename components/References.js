9d83ae8a574c9f49ea50a755adea1ab51a0b8bc5
karamanmertcan <20181701056@stu.khas.edu.tr>
2021-10-28 20:10:10 +0300

    added links

--- components/References.js
+++ components/References.js
@@ -1,36 +1,35 @@
 import React from 'react';
-import Image from 'next/image';
-
-import Letwom from '../public/images/letwom.png';
-import Plania from '../public/images/kaan-tasarim-logo.png';
-import Viskobet from '../public/images/viskobet.png';
-import SaksiVeCicek from '../public/images/saksivecicek.png';
-import MinyaturTugla from '../public/images/minyaturtuglalogo.png';
-
+import Link from 'next/link';
 const logo = [
   {
     id: 1,
-    image: '/images/letwom.png'
+    image: '/images/letwom.png',
+    link: 'https://www.letwom.com/'
   },
   {
     id: 2,
-    image: '/images/plania.png'
+    image: '/images/plania.png',
+    link: 'https://plania.co/'
   },
   {
     id: 3,
-    image: '/images/viskobet.png'
+    image: '/images/viskobet.png',
+    link: 'https://www.viskobet.com/'
   },
   {
     id: 4,
-    image: '/images/saksivecicek.png'
+    image: '/images/saksivecicek.png',
+    link: 'https://www.viskobet.com/'
   },
   {
     id: 5,
-    image: '/images/minyaturtuglalogo.png'
+    image: '/images/minyaturtuglalogo.png',
+    link: 'https://www.minyaturtugla.com/'
   },
   {
     id: 6,
-    image: '/images/metdekorasyon.png'
+    image: '/images/metdekorasyon.png',
+    link: 'https://www.metdekorasyon.com/'
   }
 ];
 
@@ -42,11 +41,15 @@ const References = () => {
           <div
             key={item.id}
             className="col-sm-12 p-3 col-md-4 reference d-flex justify-content-center">
-            <img
-              src={item.image}
-              alt=""
-              style={{ objectFit: 'contain', width: '100%', height: '100px' }}
-            />
+            <Link href={item.link}>
+              <a target="_blank">
+                <img
+                  src={item.image}
+                  alt=""
+                  style={{ objectFit: 'contain', width: '100%', height: '100px' }}
+                />
+              </a>
+            </Link>
           </div>
         ))}
       </div>
